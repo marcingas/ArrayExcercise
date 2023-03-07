edfdd4f2ff3ea24c12116fc1c7638dcb7ebe6b57
Marcin <gasiorek.mail@gmail.com>
2023-03-07 12:54:50 +0100

    add task: random sentence "Osoba urodzona w..."

--- src/pl/Marcin/ArrayScannerTask.java
+++ src/pl/Marcin/ArrayScannerTask.java
@@ -10,19 +10,19 @@ public class ArrayScannerTask {
         int arrayLength = scanner.nextInt();
         int[] numbers = new int[arrayLength];
         int i = 0;
-        while(i < numbers.length){
+        while (i < numbers.length) {
             System.out.println("podaj liczbę dla kolejnego elementu tablicy:");
             numbers[i] = scanner.nextInt();
             i++;
         }
         System.out.println("wartość indeksu zerowego tablicy: " + numbers[0]);
-        System.out.println("wartość indeksu ostatniego tablicy " + numbers[numbers.length -1]);
-        if(numbers.length % 2 == 0){
+        System.out.println("wartość indeksu ostatniego tablicy " + numbers[numbers.length - 1]);
+        if (numbers.length % 2 == 0) {
             System.out.println("Tablica nie ma środowego indeksu, mogę podać wartość dla dwóch środkowych: " +
-                    numbers[(numbers.length-1)/2] + "," + numbers[(numbers.length-1)/2 +1]);
+                    numbers[(numbers.length - 1) / 2] + "," + numbers[(numbers.length - 1) / 2 + 1]);
         }
-        if(arrayLength % 2 != 0){
-            System.out.println("Wartość środkowego indeksu tablicy wynosi: " + numbers[numbers.length/2]);
+        if (arrayLength % 2 != 0) {
+            System.out.println("Wartość środkowego indeksu tablicy wynosi: " + numbers[numbers.length / 2]);
         }
     }
 }
---
+++ src/pl/Marcin/ArrayStringThirtyOneElTask.java
@@ -0,0 +1,85 @@
+package pl.Marcin;
+
+import java.util.Random;
+
+public class ArrayStringThirtyOneElTask {
+    public static void main(String[] args) {
+        String[] table = new String[31];
+        table[0] = "Paryżu";
+        table[1] = "Wiedniu";
+        table[2] = "Krakowie";
+        table[3] = "Łodzi";
+        table[4] = "Brukseli";
+        table[5] = "Poznaniu";
+        table[6] = "Sydney";
+        table[7] = "New York";
+        table[8] = "Chicago";
+        table[9] = "Ełku";
+        table[10] = "Californi";
+        table[11] = "Żywcu";
+        table[12] = "Bielsku Białej";
+        table[13] = "Berlinie";
+        table[14] = "Cannes";
+        table[15] = "Pradze";
+        table[16] = "Sopotni Małej";
+        table[17] = "Rzeszowie";
+        table[18] = "Meksyku";
+        table[19] = "Wieliczce";
+        table[20] = "Zwardoniu";
+        table[21] = "Rajczy";
+        table[22] = "Kaliszu";
+        table[23] = "Upssali";
+        table[24] = "Watykanie";
+        table[25] = "Rzymie";
+        table[26] = "Norymberdze";
+        table[27] = "Inowrocławiu";
+        table[28] = "Jerozolimie";
+        table[29] = "Haifie";
+        table[30] = "Bershebie";
+
+        String[] month = new String[12];
+        month[0] = "Styczniu";
+        month[1] = "Lutym";
+        month[2] = "Marcu";
+        month[3] = "Kwietniu";
+        month[4] = "Maju";
+        month[5] = "Czerwcu";
+        month[6] = "Lipcu";
+        month[7] = "Sierpniu";
+        month[8] = "Wrześniu";
+        month[9] = "Październiku";
+        month[10] = "Listopadzie";
+        month[11] = "Grudniu";
+
+        String[] character = new String[20];
+        character[0] = "miła";
+        character[1] = "uczciwa";
+        character[2] = "zdolna";
+        character[3] = "uczynna";
+        character[4] = "wścibska";
+        character[5] = "podejrzliwa";
+        character[6] = "kłótliwa";
+        character[7] = "romantyczna";
+        character[8] = "skomplikowana";
+        character[9] = "toksyczna";
+        character[10] = "pracowita";
+        character[11] = "uczynna";
+        character[12] = "wrażliwa";
+        character[13] = "silna";
+        character[14] = "zaradna";
+        character[15] = "dociekliwa";
+        character[16] = "elokwentna";
+        character[17] = "inteligentna";
+        character[18] = "przystojna";
+        character[19] = "skąpa";
+
+        Random random = new Random();
+        int first = random.nextInt(30);
+        int second = random.nextInt(11);
+        int third = random.nextInt(19);
+        String a = "Osoba urodzona w ";
+        String sentence = a.concat(table[first]).concat(" w ").concat(month[second]).concat(" jest ").
+                concat(character[third]);
+        System.out.println(sentence);
+    }
+}
